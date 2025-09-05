52fc2c75fe7c102d48dd80be23bd8081653e7c0b
rushity <rushityadavds@gmail.com>
2025-09-05 18:18:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -221,7 +221,6 @@ def home():
 
     return render_template("index.html", chat_history=session["chat_history"])
 
-
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Render sets PORT
-    app.run(host="0.0.0.0", port=port)
+    app.run(debug=True)
+
